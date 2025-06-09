5c8862d29b49d922b84732b3f03ed0fcdcc7e193
Gonzalo Castro <93634423+GonzaCass@users.noreply.github.com>
2025-06-09 18:34:08 -0300

    Update start.sh
    Script que levanta los servicios en el container y copia los logs para su visualizacion local

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# ?? Asegurar existencia de logs
+#Asegurar existencia de logs
 touch /pvpgn/var/pvpgn/bnetd.log
 touch /root/.wine/drive_c/d2gs/d2gs.log
 touch /root/.wine/drive_c/d2gs/d2ge.log
 
-# ?? Iniciar servicios PvPGN con logs redirigidos
+#Iniciar servicios PvPGN con logs redirigidos
 echo "? Iniciando PvPGN..."
 /pvpgn/sbin/d2dbs -f >> /pvpgn/var/pvpgn/bnetd.log 2>&1 &
 /pvpgn/sbin/d2cs -f >> /pvpgn/var/pvpgn/bnetd.log 2>&1 &
@@ -13,7 +13,7 @@ echo "? Iniciando PvPGN..."
 
 sleep 3
 
-# ????? Iniciar D2GS con logs redirigidos (Wine)
+#Iniciar D2GS con logs redirigidos (Wine)
 /root/.wine/drive_c/d2gs/start.sh >> /root/.wine/drive_c/d2gs/d2gs.log 2>> /root/.wine/drive_c/d2gs/d2ge.log &
-# ?? Mantener proceso vivo si todo va en background
+#Mantener proceso vivo si todo va en background
 tail -f /pvpgn/var/pvpgn/bnetd.log
